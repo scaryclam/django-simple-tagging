3a91608bfb17d9db2ca85ee25efc3c03795bc39e
scaryclam <scaryclam@cortana.(none)>
2012-04-02 22:32:51 +0100

    New commit

---
+++ simple_tagging/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class TagMixin(models.Model):
+    def get_tags(self):
+        pass
+    
+
