d27ce05296f76587cf12b355d9d8891a89abbb50
Becky Lewis <github@scaryclam.co.uk>
2012-04-02 23:34:14 +0100

    Added basic setup in models

--- simple_tagging/models.py
+++ simple_tagging/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.contenttypes import generic
 
 
 class Tag(models.Model):
     tag = models.CharField(max_length=255)
-    
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
         abstract = True
 
 
 class TagMixin(models.Model):
+    tags = generic.GenericRelation(Tag)
+
     def get_tags(self):
-        pass
+        return tags
+
+    def add_tag(self, tag_name):
+        new_tag = Tag.objects.create(tag=new_tag, content_object=self)
+        
+
 
